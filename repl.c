919cc5ba3294d0620f3bd78fd3ed679f667b83e9
raventid <juliankul@gmail.com>
2015-12-11 01:12:28 +0300

    add history and editline functionality (apt-get install libedit-dev (Ubuntu 14.04))

--- repl.c
+++ repl.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <stdlib.h>
+
+#include <editline/readline.h>
+#include <editline/history.h>
 
 int main(int argc, char** argv) {
-/* Print Version and Exit Information */
 
-puts("bebe Version 0.0.0.0.1");
+puts("bebe Version 0.0.1");
 puts("Press Ctrl+c to Exit\n");
-/* In a never ending loop */
+
 while (1) {
-/* Output our prompt */fputs("bebe> ", stdout);
-/* Read a line of user input of maximum size 2048 */
-fgets(input, 2048, stdin);
-/* Echo input back to user */
-printf("No you're a %s", input);
+ char *input = readline("bebe> ");
+
+ add_history(input); 
+
+ printf("No you're a %s\n", input);
+
+ free(input);
 }
+
 return 0;
 }
