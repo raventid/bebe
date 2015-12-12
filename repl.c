b9bb34a21c8a4346a5ab6fd62ff057cd8cddde51
raventid <juliankul@gmail.com>
2015-12-12 16:04:31 +0300

    add windows compatibility for repl

--- repl.c
+++ repl.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048]
+
+/*Fake readline function*/
+char* readline(char* prompt){
+ fputs(prompt, stdout);
+ fgets(buffer, 2048, stdin);
+ char* cpy = malloc(strlen(buffer)+1);
+ strcpy(cpy, buffer);
+ cpy[strlen(cpy)-1]='\0';
+ return cpy;
+}
+
+/*Fake add_history function*/
+void add_history(char* unsued){}
+
+/*if it is not windows include the editline headers*/
+#else
 #include <editline/readline.h>
 #include <editline/history.h>
+#endif
+
 
 int main(int argc, char** argv) {
 
