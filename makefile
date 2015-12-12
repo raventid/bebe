37ad2f36ce10e4c6a0d9abeb864f62028ab9a8f6
raventid <juliankul@gmail.com>
2015-12-13 00:44:17 +0300

    add evaluation for arithmetic operations

--- makefile
+++ makefile
@@ -1,2 +1,4 @@
+CFLAGS = -std=c99
+
 repl: repl.o mpc.o
-	cc -std=c99 -Wall repl.c mpc.c -ledit -lm -o repl
+	cc $(CFLAGS) -Wall repl.c mpc.c -ledit -lm -o repl
--- repl.c
+++ repl.c
@@ -8,7 +8,7 @@
 static char buffer[2048]
 
 /*Fake readline function*/
-char* readline(char* prompt){
+char* readline(char* prompt) {
  fputs(prompt, stdout);
  fgets(buffer, 2048, stdin);
  char* cpy = malloc(strlen(buffer)+1);
@@ -18,7 +18,7 @@ char* readline(char* prompt){
 }
 
 /*Fake add_history function*/
-void add_history(char* unsued){}
+void add_history(char* unsued) {}
 
 /*if it is not windows include the editline headers*/
 #else
@@ -26,6 +26,50 @@ void add_history(char* unsued){}
 #include <editline/history.h>
 #endif
 
+int number_of_nodes(mpc_ast_t* t) {
+ if (0 == t->children_num) { return 1;}
+ /*@todo consider posibility of using else here instead of new if*/
+ if (1 <= t->children_num) {
+  int total = 1;
+  for (int i = 0; i < t->children_num; i++) {
+   total = total + number_of_nodes(t->children[i]); 
+  }
+  return total;
+ }
+ /* This is a stub for compiler and this return should never be reached*/
+ return 0;
+}
+
+/*Use operator string to see which operation to perform*/
+long eval_op(long x, char* op, long y) {
+ if (0 == strcmp(op, "+")) { return x + y; }
+ if (0 == strcmp(op, "-")) { return x - y; }
+ if (0 == strcmp(op, "*")) { return x * y; }
+ if (0 == strcmp(op, "/")) { return x / y; }
+ return 0;
+}
+
+long eval(mpc_ast_t* t) {
+ /* If tagged as number return it directly. */
+ if (strstr(t->tag, "number")) {
+  return atoi(t->contents); 
+ }
+
+ /* The operator is always second child.  */
+ char* op = t->children[1]->contents;
+
+ /* We store the third child in `x`  */
+ long x = eval(t->children[2]);
+
+ /* Iterate the remaining children and combining  */
+ int i = 3;
+ while(strstr(t->children[i]->tag, "exp")) {
+  x = eval_op(x, op, eval(t->children[i]));
+  i++;
+ }
+
+ return x;
+}
 
 int main(int argc, char** argv) {
 
@@ -37,13 +81,13 @@ mpc_parser_t* Bebe = mpc_new("bebe");
 mpca_lang(MPCA_LANG_DEFAULT,
 	"                                               \
 	number   : /-?[0-9]+/ ;                         \
-        operator : '+' | '-' | '*' | '/' ;                    \
+        operator : '+' | '-' | '*' | '/' ;              \
         expr     : <number> | '('<operator><expr>+')' ; \
         bebe     : /^/ <operator> <expr>+ /$/ ;         \
         ",	
 	Number,Operator,Expr,Bebe);
 
-puts("bebe Version 0.0.2");
+puts("bebe Version 0.0.3");
 puts("Press Ctrl+c to Exit\n");
 
 while (1) {
@@ -54,7 +98,10 @@ while (1) {
  mpc_result_t result;
  if(mpc_parse("<stdin>", input, Bebe, &result)){
   /*Success, let's print the correct AST*/
-  mpc_ast_print(result.output); 
+  long res = eval(result.output);
+  printf("%li\n", res);
+  //we might output ast tree structure while debbuging, perhaps make a constant here
+  //mpc_ast_print(result.output); 
   mpc_ast_delete(result.output);
  } else {
   /*Failure, let's print what went wrong*/
